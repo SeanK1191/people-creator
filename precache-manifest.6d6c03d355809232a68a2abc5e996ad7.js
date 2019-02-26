fadb392ddce78baccf01abfd1e9bd0a17421469f
Sean Kennedy <sean-kennedy1191@hotmail.co.uk>
2019-02-26 21:46:11 +0000

    Updates

---
+++ precache-manifest.6d6c03d355809232a68a2abc5e996ad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e2d6b4193a19cf0569c8",
+    "url": "/static/js/main.e2d6b419.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "e2d6b4193a19cf0569c8",
+    "url": "/static/css/main.98a4d367.chunk.css"
+  },
+  {
+    "revision": "7422401b2514072f451df03842689f39",
+    "url": "/index.html"
+  }
+];
---
+++ service-worker-custom.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-restricted-globals */
+console.log('My custom service worker')
+
+let selectedPeople = [];
+
+// Install Service Worker
+self.addEventListener('install', function(event){
+    console.log('installed!');
+});
+
+// Service Worker Active
+self.addEventListener('activate', function(event){
+    console.log('activated!');
+});
+
+self.addEventListener('message', function(event){
+    if(event.data.eventType === 'personSelected') {
+        selectedPeople.push(event.data.person);
+    }
+
+    if(event.data.eventType === 'personUnselected') {
+        selectedPeople = selectedPeople.filter((element) => {
+            return element.Id !== event.data.person.Id;
+        });
+    }
+
+    if(event.data.eventType === 'getSelectedPeople') {
+        event.ports[0].postMessage(selectedPeople);
+    }
+    
+});
