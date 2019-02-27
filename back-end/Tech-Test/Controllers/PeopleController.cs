78c2b37095c5212d91b9bb80ec55dc2ed3db0808
Sean Kennedy <sean-kennedy1191@hotmail.co.uk>
2019-02-27 20:40:56 +0000

    Changed background colours for sortable headers.
    Enabled CORS on the API so it can work when running locally.

--- back-end/Tech-Test/Controllers/PeopleController.cs
+++ back-end/Tech-Test/Controllers/PeopleController.cs
@@ -3,11 +3,13 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using Tech_Test.Models;
 
 namespace Tech_Test.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     [RoutePrefix("")]
     public class PeopleController : ApiController
     {
--- front-end/src/components/PeopleTable.js
+++ front-end/src/components/PeopleTable.js
@@ -13,10 +13,10 @@ const PeopleTable = function({ people, personSelected, personUnselected, sortPeo
             <table>
                 <tbody>
                     <tr>
-                        <th style={{ cursor: 'pointer'  }} onClick={() => { sortPeople('name') }}>{nameHeader}</th>
+                        <th style={{ cursor: 'pointer', backgroundColor: 'LightGray'  }} onClick={() => { sortPeople('name') }}>{nameHeader}</th>
                         <th>Age</th>
                         <th>Balance</th>
-                        <th style={{ cursor: 'pointer'  }} onClick={() => { sortPeople('email') }}>{emailHeader}</th>
+                        <th style={{ cursor: 'pointer', backgroundColor: 'LightGray'  }} onClick={() => { sortPeople('email') }}>{emailHeader}</th>
                         <th>Address</th>
                     </tr>
                     {
